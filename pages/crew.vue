7137a6b133bee314feb9b008e3c34f0b16b9b8d8
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-02-25 10:24:27 +0100

    Update crew.vue

--- pages/crew.vue
+++ pages/crew.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Crew</h1>
+  <ul>
+    <li v-for="member in crew" :key="member.id">
+      {{ member.title.rendered }}
+    </li>
+  </ul>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      crew: [],
+    }
+  },
+  methods: {
+    async loadData() {
+      const response = await fetch(
+        'https://space.wp.seiwald.hak-stjo.schulwebspace.at/wp-json/wp/v2/crew'
+      )
+      this.crew = await response.json()
+    },
+  },
+  mounted() {
+    this.loadData()
+    console.log('mounted')
+  },
+}
+</script>
